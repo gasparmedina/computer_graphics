e09ffd0611766d76a470ee0f5f2c151533946ccf
gasparmedina <gasweb456@gmail.com>
2017-10-16 21:33:00 -0300

    Ejercicio 7

--- Practico_0/exercise_7.c
+++ Practico_0/exercise_7.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct Vec3 {
+
+ int a;
+
+};
+
 void exercise7(){
 
+    struct Vec3 miVec1;
+    struct Vec3 miVec2;
+    miVec1.a = 2;
+    miVec2.a = 2;
+
+    int result = add(miVec1, miVec2);
+    printf("REsultado ejercicio 7 %d", result);
+
+}
+
+int add (struct Vec3 vec1, struct Vec3 vec2){
+
+    return vec1.a + vec2.a;
 
 }
